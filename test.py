d287334faa556c37cc8dfea7bd12ae6b310cedf6
Arjun Nain <arjunnain1234@gmail.com>
2026-09-04 03:10:53 +0530

    Initial commit

---
+++ test.py
@@ -0,0 +1,17 @@
+# print(1+1);
+
+## Learn more about Python at https://www.python.org/doc/
+
+# '''
+# lorem ipsum dolor sit amet d d fd dfjklh sadjks dafjskdbh
+
+# '''
+
+# name = 'Arjun Nain'
+# age = 24
+# _codingAptitude = input("Enter your coding aptitude score: ")
+
+# print("This is the type: ",type(_codingAptitude))
+
+result = int('5') + 5
+print(result)
